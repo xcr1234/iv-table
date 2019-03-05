c38c716fbd030bf303c7905304bef48cf6690e16
xiecr002 <xiecr002@foresealife.com>
2019-03-05 12:59:25 +0800

    修改useBuiltIns选项，去除不必要的polyfills

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      // 传入 useBuiltIns: false 选项
+      // https://cli.vuejs.org/zh/guide/browser-compatibility.html#%E6%9E%84%E5%BB%BA%E5%BA%93%E6%88%96%E6%98%AF-web-component-%E6%97%B6%E7%9A%84-polyfills
+      'useBuiltIns': false
+    }]
   ]
 }
